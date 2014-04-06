8af59ca8ac43e7c5b28173565e1727c6edf667c0
Philippe Vienne <Philippe.Vienne@insa-lyon.fr>
2014-04-06 11:27:33 +0200

    Start working on factories

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,6 @@
 # See http://railsapps.github.io/rails-environment-variables.html
 
 if Rails.env == 'development' # Add test data for dev
-  puts 'Fake data'
-  # Add building1 to building5 as demo building
-  for i in 1..5 do
-    Building.create(:name=>'building'+i.to_s) if Building.find_by_name('building'+i.to_s).nil?
-  end
-  buildings_for_machines = (1..5).to_a.shuffle![0..7]
+  puts 'Demo data'
 
-  # Add machine1 to machine8 as demo machine
-  for i in 1..8 do
-    Machine.create!(
-        :name=>'machine'+i.to_s,
-        :blocked=>false,
-        :building=> Building.find_by_name('building'+buildings_for_machines[i].to_s)
-    ) if Machine.find_by_name('machine'+i.to_s).nil?
-  end
 end
